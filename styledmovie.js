895f80c8221395a8294fdbb680eed796a047ec69
Yunshu Gao <yug129@ucsd.edu>
2019-08-03 05:31:53 -0700

    Fixed dialog position and add movie DOM issue

--- styledmovie.js
+++ styledmovie.js
@@ -3,16 +3,16 @@
 export var movieList = [];
 export var curLoad = 0;
 
-//added temp access token
-//const atoken = "ufVj4Aymv4wyCjwEyp7pKsSkrQiLwLh4QTEv5XEGk1KMaASKMTJxD5zvgqrRemde";
-var userLoginInfoStr = localStorage.getItem("userLoginInfo");
-var userLoginInfo = JSON.parse(userLoginInfoStr);
-if (userLoginInfo == null){
-  userLoginInfo = {id: "unauthorized", username: "Unauthorized User"};
+// added temp access token
+// const atoken = "ufVj4Aymv4wyCjwEyp7pKsSkrQiLwLh4QTEv5XEGk1KMaASKMTJxD5zvgqrRemde";
+const userLoginInfoStr = localStorage.getItem('userLoginInfo');
+let userLoginInfo = JSON.parse(userLoginInfoStr);
+if (userLoginInfo == null) {
+  userLoginInfo = {id: 'unauthorized', username: 'Unauthorized User'};
   redirectUnauthorizedUser();
 }
 const atoken = userLoginInfo.id;
-export const username = localStorage.getItem("userName");
+export const username = localStorage.getItem('userName');
 
 // Library functions
 
@@ -47,12 +47,12 @@ export function appendListNode(node, list, index) {
   genre.innerHTML = node.genre;
   const userRating = listNode.querySelector('.card-userRating');
   // userRating.innerHTML = `User Rating: ${node.userRating}`;
-  for (var i = 0; i<node.userRating; i++){
-    userRating.children[i].setAttribute("class", "fa fa-star checked");
+  for (let i = 0; i<node.userRating; i++) {
+    userRating.children[i].setAttribute('class', 'fa fa-star checked');
   }
   const image = listNode.querySelector('.card-image');
-  image.setAttribute("src", node.image);
-  //image.innerHTML = node.image;
+  image.setAttribute('src', node.image);
+  // image.innerHTML = node.image;
   // Finish the list node
   initializeListNodeButtons(index, listNode);
   listNode.querySelector('.card').setAttribute('id', `li-${index}`);
@@ -80,11 +80,11 @@ export function initializeListNodeButtons(index, node) {
  * @param - index: the index of the element in the movie array
  */
 export function edit(index) {
-  //make sure the fields are white
-  let title = document.getElementById('title');
-  let year = document.getElementById('year');
-  let rating = document.getElementById('rating');
-  let image = document.getElementById('image');
+  // make sure the fields are white
+  const title = document.getElementById('title');
+  const year = document.getElementById('year');
+  const rating = document.getElementById('rating');
+  const image = document.getElementById('image');
   title.style.borderColor = 'white';
   title.style.borderWidth = '1px';
   year.style.borderColor = 'white';
@@ -94,7 +94,7 @@ export function edit(index) {
   image.style.borderColor = 'white';
   image.style.borderWidth = '1px';
   // Preprocess the dialog
-  var editSaveButton = document.getElementById('save');
+  const editSaveButton = document.getElementById('save');
   editSaveButton.setAttribute('onclick', `setContent(${index})`);
   // Get the current values
   const curTitle = movieList[index].title;
@@ -120,30 +120,30 @@ export function edit(index) {
  */
 export function setContent(index) {
   // Get user input
-  let title = document.getElementById('title');
-  let year = document.getElementById('year');
-  let rating = document.getElementById('rating');
-  let genre = document.getElementById('genre');
-  let userRating = document.getElementById('userRating');
-  let image = document.getElementById('image');
+  const title = document.getElementById('title');
+  const year = document.getElementById('year');
+  const rating = document.getElementById('rating');
+  const genre = document.getElementById('genre');
+  const userRating = document.getElementById('userRating');
+  const image = document.getElementById('image');
   // Check for empty input
-  if (title.value == '' || year.value  == '' || rating.value  == '' || image.value  == '') {
+  if (title.value == '' || year.value == '' || rating.value == '' || image.value == '') {
     // Generate error message
     const errText = 'All fields are required.';
     document.getElementById('err').innerHTML = errText;
-    if (title.value  == '') {
+    if (title.value == '') {
       title.style.borderColor = 'rgba(242, 88, 124, 0.60)';
       title.style.borderWidth = '3px';
     }
-    if (year.value  == '') {
+    if (year.value == '') {
       year.style.borderColor = 'rgba(242, 88, 124, 0.60)';
       year.style.borderWidth = '3px';
     }
-    if (rating.value  == '') {
+    if (rating.value == '') {
       rating.style.borderColor = 'rgba(242, 88, 124, 0.60)';
       rating.style.borderWidth = '3px';
     }
-    if (image.value  == '') {
+    if (image.value == '') {
       image.style.borderColor = 'rgba(242, 88, 124, 0.60)';
       image.style.borderWidth = '3px';
     }
@@ -151,32 +151,32 @@ export function setContent(index) {
   } else {
     if (userRating < 0 || userRating > 5) {
       const ratingErr = 'Please choose user rating from 0 to 5.';
-    document.getElementById('err').innerHTML = ratingErr;
-    return;
+      document.getElementById('err').innerHTML = ratingErr;
+      return;
     }
     // Sanitize input
-    title.value  = DOMPurify.sanitize(title.value );
-    year.value  = DOMPurify.sanitize(year.value );
-    rating.value  = DOMPurify.sanitize(rating.value );
-    genre.value  = DOMPurify.sanitize(genre.value );
-    userRating.value  = DOMPurify.sanitize(userRating.value );
-    image.value  = DOMPurify.sanitize(image.value );
+    title.value = DOMPurify.sanitize(title.value );
+    year.value = DOMPurify.sanitize(year.value );
+    rating.value = DOMPurify.sanitize(rating.value );
+    genre.value = DOMPurify.sanitize(genre.value );
+    userRating.value = DOMPurify.sanitize(userRating.value );
+    image.value = DOMPurify.sanitize(image.value );
   }
   // Set Content in remote
   const listNodeCur = document.getElementById(`li-${index}`);
   // Check if the node exists
-  var node;
+  let node;
   if (listNodeCur != null) {
-    let id = movieList[index].id;
+    const id = movieList[index].id;
     node = {
-      "title": title.value , "year": year.value , "rating": rating.value , "genre": genre.value ,
-      "userRating": userRating.value , "image": image.value , "id": id
+      'title': title.value, 'year': year.value, 'rating': rating.value, 'genre': genre.value,
+      'userRating': userRating.value, 'image': image.value, 'id': id,
     };
     setContentRemote(node, index, true);
   } else {
     node = {
-      "title": title.value , "year": year.value , "rating": rating.value , "genre": genre.value ,
-      "userRating": userRating.value , "image": image.value 
+      'title': title.value, 'year': year.value, 'rating': rating.value, 'genre': genre.value,
+      'userRating': userRating.value, 'image': image.value,
     };
     setContentRemote(node, index, false);
   }
@@ -186,8 +186,8 @@ export function setContent(index) {
  * setContentRemote(node)
  */
 export function setContentRemote(node, index, editing) {
-  let xhr = new XMLHttpRequest();
-  var endpoint
+  const xhr = new XMLHttpRequest();
+  let endpoint;
   console.log(editing);
   if (editing) {
     console.log(node.id);
@@ -195,35 +195,33 @@ export function setContentRemote(node, index, editing) {
   } else {
     endpoint = `http://introweb.tech/api/movies?access_token=${atoken}`;
   }
-  var params = typeof node == 'string' ? data : Object.keys(node).map(
-    function (k) { return encodeURIComponent(k) + '=' + encodeURIComponent(node[k]) }
+  const params = typeof node == 'string' ? data : Object.keys(node).map(
+      function(k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(node[k]);
+      }
   ).join('&');
-  xhr.open("POST", endpoint);
-  xhr.onreadystatechange = function () {
+  xhr.open('POST', endpoint);
+  xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       // Update the DOM differently when editing
       if (!editing) {
         // Update data structure
         movieList[index] = JSON.parse(this.responseText);
-        console.log(movieList[index]);
-        const list = document.getElementById('list');
-        appendListNode(movieList[index], list, index);
+        loadMovieList(loadContent, Math.max(3, index+1));
       } else {
-        loadMovieList(loadContent, Math.max(3,index+1));
+        loadMovieList(loadContent, Math.max(3, index+1));
       }
       // Colse and clear dialog
       document.getElementById('edit-dialog').close();
       clearDialog();
       // Save to browser storage
       saveMovieList();
-    }
-    else if (this.status != 200) {
+    } else if (this.status != 200) {
       console.log(`Set content error: ${this.status}`);
+    } else {
+      console.log('request in progress!');
     }
-    else {
-      console.log("request in progress!");
-    }
-  }
+  };
   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.send(params);
@@ -248,24 +246,23 @@ export function remove(index) {
  */
 export function removeContent(index) {
   // Remove content on remote
-  let xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   const endpoint = `http://introweb.tech/api/movies/${movieList[index].id}?access_token=${atoken}`;
-  xhr.open("DELETE", endpoint);
-  xhr.onreadystatechange = function () {
+  xhr.open('DELETE', endpoint);
+  xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       movieList.splice(index, 1);
       // Close dialog
       document.getElementById('remove-dialog').close();
       // Reload content
       saveMovieList();
-      loadMovieList(loadContent, Math.max(3,index+1));
+      loadMovieList(loadContent, Math.max(3, index+1));
     } else if (this.status != 200) {
       console.log(`Delete error: ${this.status}`);
+    } else {
+      console.log('request in progress!');
     }
-    else {
-      console.log("request in progress!");
-    }
-  }
+  };
   xhr.send();
 }
 
@@ -275,11 +272,11 @@ export function removeContent(index) {
  * @param - index: the index of the element in the movie array
  */
 export function add() {
-  //make sure the fields are white
-  let title = document.getElementById('title');
-  let year = document.getElementById('year');
-  let rating = document.getElementById('rating');
-  let image = document.getElementById('image');
+  // make sure the fields are white
+  const title = document.getElementById('title');
+  const year = document.getElementById('year');
+  const rating = document.getElementById('rating');
+  const image = document.getElementById('image');
   title.style.borderColor = 'white';
   title.style.borderWidth = '1px';
   year.style.borderColor = 'white';
@@ -289,7 +286,7 @@ export function add() {
   image.style.borderColor = 'white';
   image.style.borderWidth = '1px';
   // Preprocess the dialog
-  var editSaveButton = document.getElementById('save');
+  const editSaveButton = document.getElementById('save');
   editSaveButton.setAttribute('onclick', `setContent(${movieList.length})`);
   // Show the dialog
   document.getElementById('edit-dialog').show();
@@ -328,7 +325,7 @@ export function clearContent() {
  * Called whtn the data structre is updated
  */
 export function saveMovieList() {
-  var listStr = JSON.stringify(movieList);
+  const listStr = JSON.stringify(movieList);
   localStorage.setItem('movieList-s', listStr);
 }
 
@@ -338,15 +335,15 @@ export function saveMovieList() {
  * Called when page loads
  */
 export function loadMovieList(callback, num) {
-  var listStr;
-  let xhr = new XMLHttpRequest();
+  let listStr;
+  const xhr = new XMLHttpRequest();
   const endpoint = `http://introweb.tech/api/movies/movieList?access_token=${atoken}`;
-  xhr.open("GET", endpoint);
-  //xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  xhr.open('GET', endpoint);
+  // xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
   xhr.send();
-  xhr.onreadystatechange = function () {
-    //console.log(this.readyState);
-    //console.log(this.status);
+  xhr.onreadystatechange = function() {
+    // console.log(this.readyState);
+    // console.log(this.status);
     if (this.readyState == 4 && this.status == 200) {
       listStr = xhr.responseText;
       if (listStr == null) {
@@ -356,9 +353,9 @@ export function loadMovieList(callback, num) {
       }
     } else {
       if (this.status != 200) {
-        console.log("Error: could not access movie list!");
+        console.log('Error: could not access movie list!');
       } else {
-        console.log("Request in progress!");
+        console.log('Request in progress!');
       }
     }
 
@@ -371,29 +368,29 @@ export function loadMovieList(callback, num) {
       movieList = movieList.movies;
       callback(num);
     }
-  }
+  };
 }
 /*
- * logout 
+ * logout
  */
 export function logout() {
-  let endpoint = `http://introweb.tech/api/Users/logout?access_token=${atoken}`;
-  let xhr = new XMLHttpRequest();
+  const endpoint = `http://introweb.tech/api/Users/logout?access_token=${atoken}`;
+  const xhr = new XMLHttpRequest();
   xhr.open('POST', endpoint, true);
-  xhr.onreadystatechange = function () {
-    localStorage.removeItem("userLoginInfo");
-    window.location = "./signout.html";
-  }
+  xhr.onreadystatechange = function() {
+    localStorage.removeItem('userLoginInfo');
+    window.location = './signout.html';
+  };
   xhr.send();
 }
 
 /*
  * redirectUnauthorizedUser
  * Hide the content and shows a notification of redirection instead
- * Redirect to login page 
+ * Redirect to login page
  */
 export function redirectUnauthorizedUser() {
-  window.location.replace("./signup.html");
+  window.location.replace('./signup.html');
 }
 
 /*
@@ -419,7 +416,7 @@ export function clearDialog() {
 //   document.getElementById('add-button').addEventListener('click', function () {
 //     add();
 //   });
-  
+
 //   document.getElementById('nav-login-control').addEventListener('click', function () {
 //     logout();
 //   });
